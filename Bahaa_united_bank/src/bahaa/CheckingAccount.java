0c4485fbcef7a164752aa73109c225dc8373b5da
bbah2 <bbah2@DESKTOP-77MKSN3>
2025-12-05 18:21:54 +0300

    added withdraw limit and credit limit to savings and checking classes

--- Bahaa_united_bank/src/bahaa/CheckingAccount.java
+++ Bahaa_united_bank/src/bahaa/CheckingAccount.java
@@ -6,6 +6,14 @@ public class CheckingAccount extends Account {
         super(initialBalance); 
     }
 
-    
+    @Override
+    // creating a creadit limit hmm lets say 50
+    public boolean withdraw(double amount) {
+    	if (checkBalance() - amount < -50) {
+    		System.out.println("Credit limit is reached go get a real job");
+    		return false;
+    	}
+    	return super.withdraw(amount);
+    }
     
 }
--- Bahaa_united_bank/src/bahaa/SavingsAccount.java
+++ Bahaa_united_bank/src/bahaa/SavingsAccount.java
@@ -4,6 +4,15 @@ public class SavingsAccount extends Account {
 	public SavingsAccount(double initialBalance) {
 	    super(initialBalance);
 	}
+	@Override 
+	public boolean withdraw(double amount) {
+		// I will make it 1000 but it change later based on my mood
+		if(amount > 1000) {
+			System.out.println("You can not do that");
+			return false;
+		}
+		return super.withdraw(amount);
+	}
 
 
 }
