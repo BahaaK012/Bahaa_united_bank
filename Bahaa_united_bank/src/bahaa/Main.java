b27d9ac309886595105de18a5e8caa7db0cf1256
bbah2 <bbah2@DESKTOP-77MKSN3>
2025-12-17 10:06:53 +0300

    edited the main to test the username and password (it worked but i still need to add rest)

--- Bahaa_united_bank/src/bahaa/Main.java
+++ Bahaa_united_bank/src/bahaa/Main.java
@@ -1,68 +1,43 @@
 package bahaa;
+
 import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Welcome to Bahaa United Bank. Here we give our clients the best exp");
-		Bank bank = new Bank();
-		bank.addAccount(new SavingsAccount(1, "bahaa", "1234", 5000));
-		bank.addAccount(new CheckingAccount(2, "ali", "abcd", 100));
-	        System.out.println("Choose your account: ");
-	        System.out.println("0 - Savings Account");
-	        System.out.println("1 - Checking Account");
-	        int accIndex = input.nextInt();
+    public static void main(String[] args) {
 
-	        Account userAcc = bank.getAccount(accIndex);
+        Bank bank = new Bank();
 
-	        System.out.println("You logged into account #" + accIndex);
+        // create ONE user with TWO accounts
+        bank.addAccount(new SavingsAccount(1, "bahaa", "1234", 5000));
+        bank.addAccount(new CheckingAccount(1, "bahaa", "1234", 100));
 
-	        // --- MENU ---
-	        System.out.println("Choose an operation:");
-	        System.out.println("1 - Deposit");
-	        System.out.println("2 - Withdraw");
-	        System.out.println("3 - Transfer");
-	        System.out.println("4 - View Transactions");
+        Scanner sc = new Scanner(System.in);
 
-	        int choice = input.nextInt();
+        System.out.print("Username: ");
+        String username = sc.nextLine();
 
-	        switch(choice) {
+        System.out.print("Password: ");
+        String password = sc.nextLine();
 
-	            case 1:
-	                System.out.print("Enter amount to deposit: ");
-	                double d = input.nextDouble();
-	                userAcc.deposit(d);
-	                break;
+        Account[] userAccounts = bank.loginUser(username, password);
 
-	            case 2:
-	                System.out.print("Enter amount to withdraw: ");
-	                double w = input.nextDouble();
-	                userAcc.withdraw(w);
-	                break;
+        if (userAccounts == null) {
+            System.out.println("Login failed");
+            return;
+        }
 
-	            case 3:
-	                System.out.print("Transfer to which account? ");
-	                int toIndex = input.nextInt();
-	                System.out.print("Enter amount: ");
-	                double t = input.nextDouble();
-	                userAcc.transfer(bank.getAccount(toIndex), t);
-	                break;
+        System.out.println("Login successful");
+    
 
-	            case 4:
-	                System.out.println("Transaction history:");
-	                userAcc.printTransactions();
-	                break;
+System.out.println("Choose account:");
+System.out.println("0 - Savings");
+System.out.println("1 - Checking");
 
-	            default:
-	                System.out.println("Invalid choice.");
-	        }
+int choice = sc.nextInt();
+Account currentAccount = userAccounts[choice];
 
-	   
-
-	        bank.printAccounts();
-	    }
-	
-	}
-
-
+System.out.println("You logged into " +
+    (currentAccount instanceof SavingsAccount ? "Savings" : "Checking"));
+}
+}
