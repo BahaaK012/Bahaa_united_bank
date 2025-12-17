ab6b5d1ee82360d0aedb427839ac0059779cfff2
bbah2 <bbah2@DESKTOP-77MKSN3>
2025-12-17 10:22:16 +0300

    ok I finally made it work, now the user must login first then he can do opreations, they are now all saved in one text file

--- Bahaa_united_bank/src/bahaa/Main.java
+++ Bahaa_united_bank/src/bahaa/Main.java
@@ -39,5 +39,49 @@ Account currentAccount = userAccounts[choice];
 
 System.out.println("You logged into " +
     (currentAccount instanceof SavingsAccount ? "Savings" : "Checking"));
+
+System.out.println("Choose operation:");
+System.out.println("1 - Deposit");
+System.out.println("2 - Withdraw");
+System.out.println("3 - Transfer");
+System.out.println("4 - View Transactions");
+
+int choice1 = sc.nextInt();
+
+switch (choice1) {
+
+    case 1:
+        System.out.print("Enter amount to deposit: ");
+        double d = sc.nextDouble();
+        currentAccount.deposit(d);
+        System.out.println("Deposit successful");
+        break;
+
+    case 2:
+        System.out.print("Enter amount to withdraw: ");
+        double w = sc.nextDouble();
+        if (currentAccount.withdraw(w))
+            System.out.println("Withdraw successful");
+        else
+            System.out.println("Withdraw failed");
+        break;
+
+    case 3:
+        System.out.print("Transfer to (0 or 1): ");
+        int to = sc.nextInt();
+        System.out.print("Enter amount: ");
+        double t = sc.nextDouble();
+        currentAccount.transfer(userAccounts[to], t);
+        break;
+
+    case 4:
+        currentAccount.printTransactions();
+        break;
+
+    default:
+        System.out.println("Invalid choice");
+}
+
+System.out.println("Current balance: " + currentAccount.checkBalance());
 }
 }
